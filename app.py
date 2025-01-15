372596b144e7ce019b547193a5773dee83aa8421
Aditya Mittal <adityamittal2410@gmail.com>
2025-01-16 02:39:47 +0530

    add resume parser

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from extract_text import extract_text_from_pdf
+from resume_parser.resumeparse import resumeparse
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +16,8 @@ def upload_resume():
 
     # Extract text from the uploaded PDF
     extracted_text = extract_text_from_pdf(file)
-    return jsonify({"extracted_data": extracted_text}), 200
+    data = resumeparse.read_file(file)
+    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(port=8000,debug=True)
